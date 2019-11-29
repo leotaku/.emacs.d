a4944e94826677002d891ded4e75a44c84030c24
leotaku <leo.gaskin@brg-feldkirchen.at>
2019-11-29 23:40:19 +0100

    Remove unused file

--- lisp/visual-regexp.el
+++
@@ -1,9 +0,0 @@
-(defun vr-test ()
-  (interactive)
-  (save-excursion
-    (cl-destructuring-bind
-        (regexp replace _ __)
-        (vr--interactive-get-args
-         'vr--mode-regexp-replace
-         nil)
-      (replace-regexp (pcre-to-elisp regexp) replace nil (point-min) (point-max)))))
