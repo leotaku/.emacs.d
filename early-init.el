9be8c97f93871093bab7221ce7a69b955c8cf2ff
leotaku <leo.gaskin@brg-feldkirchen.at>
2020-01-07 19:19:52 +0100

    Spacing

--- early-init.el
+++ early-init.el
@@ -12,9 +12,11 @@
 ;;; Code:
 
 ;; Never load outdated bytecode (why is this not the default?)
+
 (setq load-prefer-newer t)
 
 ;; Defer garbage collection further back in the startup process
+
 (let ((normal-gc-cons-threshold (* 8 1024 1024))
       (init-gc-cons-threshold (* 256 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
@@ -23,18 +25,22 @@
    (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;; Debug garbage collection performance
+
 (setq garbage-collection-messages nil)
 
 ;; Package initialization normally occurs automatically, but this can
 ;; be unset in the `early-init-file'.
+
 (setq package-enable-at-startup nil)
 
 ;; Prevent the glimpse of un-styled Emacs by setting these early
+
 (add-to-list 'default-frame-alist '(tool-bar-lines . 0))
 (add-to-list 'default-frame-alist '(menu-bar-lines . 0))
 (add-to-list 'default-frame-alist '(vertical-scroll-bars))
 
 ;; One less file to load at startup
+
 (setq site-run-file nil)
 
 ;; Load straight and packages
