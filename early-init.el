87930e0f1fa83008b8c22095eeb26307eca4765d
leotaku <leo.gaskin@brg-feldkirchen.at>
2020-01-07 19:19:38 +0100

    Split early init file

--- early-init.el
+++ early-init.el
@@ -37,52 +37,11 @@
 ;; One less file to load at startup
 (setq site-run-file nil)
 
-;; Load benchmarking utility from local dir, bypassing `straight.el'
-(let ((dir (expand-file-name
-            "straight/build/benchmark-init"
-            user-emacs-directory)))
-  (if (file-directory-p dir)
-      (progn
-        (add-to-list 'load-path dir)
-        (require 'benchmark-init)
-        (benchmark-init/activate)
-        (add-hook 'after-init-hook 'benchmark-init/deactivate))
-    (warn "The benchmark-init package is missing from your straight directory!")))
+;; Load straight and packages
 
-;; Emacs wants to load `package.el' before the init file,
-;; so we do the same with `straight.el'
-(setq straight-fix-org t
-      straight-enable-use-package-integration nil
-      straight-check-for-modifications '(find-when-checking check-on-save))
-
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(require 'straight)
-(require 'straight-x)
-
-;; `package-set.el' loading mechanism
-(let* ((contents
-        (with-current-buffer
-            (let ((default-directory user-emacs-directory))
-              (find-file-noselect "package-set.el"))
-          (goto-char (point-min))
-          (prog1
-              (read (current-buffer))
-            (kill-buffer (current-buffer)))))
-       (repos (nth 0 contents))
-       (packages (nth 1 contents)))
-  (setq straight-recipe-repositories (append repos nil))
-  (mapc 'straight-use-package packages))
+(load-file
+ (expand-file-name "load-packages.el"
+		   user-emacs-directory))
 
 (provide 'early-init)
 
