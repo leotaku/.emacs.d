2cb58ffa64b98def9a9efccafe014ff76134e165
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-04-25 10:12:31 +0200

    Remove sharp quote

--- init.el
+++ init.el
@@ -169,7 +169,7 @@
   :config
   (setq flymake-diagnostic-at-point-display-diagnostic-function
         'flymake-diagnostic-at-point-display-minibuffer)
-  (add-hook 'flymake-mode-hook #'flymake-diagnostic-at-point-mode))
+  (add-hook 'flymake-mode-hook 'flymake-diagnostic-at-point-mode))
 
 (bk-block notmuch
   :wanted-by delayed-target
