a2c924c201b714750ab802e0bd3369932b4a1dc2
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-10-16 23:12:26 +0200

    Restrict ace-window scope

--- init.el
+++ init.el
@@ -96,6 +96,8 @@
          ("C-x j" . delete-other-windows)
          ("C-x d" . kill-buffer)
          ("C-x k" . delete-window-or-frame))
+  :custom
+  (aw-scope . 'visible)
   :config
   (advice-add 'keyboard-quit :around 'advice-keyboard-quit)
   (defun advice-keyboard-quit (func)
