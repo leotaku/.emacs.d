d3f985f74f30a8bfb9fe419a4a91eadae16cf6e6
Leo Gaskin <leo.gaskin@le0.gs>
2021-05-05 21:13:27 +0200

    Commit suppress byte compile warnings

--- etc/custom.el
+++ etc/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(warning-suppress-log-types '((comp)))
+ '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
